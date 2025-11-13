b28847009bc6fd9b56170659861af92041da1d86
saravanabharathi07 <146365581+saravanabharathi07@users.noreply.github.com>
2025-11-13 17:23:57 +0530

    Delete learnpython directory

--- learnpython/statistics library.py
+++
@@ -1,2 +0,0 @@
-import statistics
-print(statistics.mean([100,80,50,89,39]))
--- learnpython/vote.py
+++
@@ -1,9 +0,0 @@
-import sys
-def vote():
-    num="18"
-    if sys.argv[1]==num:
-        sys.exit("You are eligible to vote")
-    else:
-        sys.exit("You are not eligible to vote")
-vote()
-
